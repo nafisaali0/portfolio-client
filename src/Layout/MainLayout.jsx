455f3b33ce341cd5be889636ea5dd6c4f28935b8
nafisaali0 <nafisaali20006@gmail.com>
2025-01-19 03:14:18 +0600

    add favicon

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../shared/Navbar/Navbar"
 
 const MainLayout = () => {
     return (
         <>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </>
     )
