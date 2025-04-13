c5f88245ad23f15450587223465a1932189839fb
nafisaali0 <nafisaali20006@gmail.com>
2025-04-13 07:20:22 +0600

    change skill's color

--- src/page/Home/Skills/Skills.jsx
+++ src/page/Home/Skills/Skills.jsx
@@ -7,7 +7,6 @@ import express from "../../../../public/techIcon/express-new.svg"
 import mongoDB from "../../../../public/techIcon/mongodb.svg"
 import figma from "../../../../public/tools/figma.svg"
 import vscode from "../../../../public/tools/vs-code.svg"
-// import vercel from "../../../../public/tools/vercel.svg"
 import { TbBrandVercelFilled } from "react-icons/tb";
 import netlify from "../../../../public/tools/netlify.svg"
 import github from "../../../../public/tools/github.svg"
@@ -19,7 +18,7 @@ const Skills = () => {
 
     const frontendSkills = [
         { name: "JavaScript", image: javaScript },
-        { name: "React.js", image: react },
+        { name: "React", image: react },
         { name: "HTML", image: html },
         { name: "CSS", image: css },
         { name: "Tailwind", image: tailwind },
@@ -42,26 +41,26 @@ const Skills = () => {
     return (
         <>
             <div className="py-20 px-10">
-                <h2 className="text-2xl font-bold uppercase mb-6">Tech   Arsenal</h2>
+                <h2 className="text-2xl font-semibold uppercase mb-6">Tech Arsenal</h2>
                 {/* tab */}
                 <Tabs onSelect={(index) => handleTabSelect(index)}>
                     <TabList className={"cursor-pointer flex gap-5 items-center text-2xl font-semibold my-5 text-black dark:text-white"}>
-                        <Tab className={`${selectedTab === 0 ? 'p-2 border-b-2 outline-none bg-white dark:bg-transparent dark:text-white' : ''}`}>FrontEnd</Tab>
-                        <Tab className={`${selectedTab === 1 ? 'p-2 border-b-2 outline-none bg-white dark:bg-transparent dark:text-white' : ''}`}>Backend</Tab>
+                        <Tab className={`${selectedTab === 0 ? 'p-2 border-b-2 outline-none bg-white dark:bg-transparent dark:text-white' : ''}`}>Front End</Tab>
+                        <Tab className={`${selectedTab === 1 ? 'p-2 border-b-2 outline-none bg-white dark:bg-transparent dark:text-white' : ''}`}>Back End</Tab>
                         <Tab className={`${selectedTab === 2 ? 'p-2 border-b-2 outline-none bg-white dark:bg-transparent dark:text-white' : ''}`}>Tools</Tab>
                     </TabList>
 
-                    <div className="my-5 font-medium">
+                    <div className="my-5 font-medium text-sm text-[#0D1117] dark:text-[#9F9F9F]">
                         <TabPanel>
                             <div className="grid grid-cols-3 sm:grid-cols-4 gap-4">
                                 <>
                                     {frontendSkills.map((skills, index) => (
                                         <div
                                             key={index}
-                                            className="flex flex-col items-center justify-center px-4 py-6 border border-[#64748B] rounded-xl"
+                                            className="flex flex-col items-center justify-center px-4 py-6 rounded-xl border border-[#64748B]"
                                         >
                                             <img src={skills.image} className="w-8 h-8" />
-                                            <span className="mt-2 text-sm dark:text-[#9F9F9F]">{skills.name}</span>
+                                            <span className="mt-2">{skills.name}</span>
                                         </div>
                                     ))}
                                 </>
@@ -73,10 +72,10 @@ const Skills = () => {
                                     {backendSkills.map((skills, index) => (
                                         <div
                                             key={index}
-                                            className="flex flex-col items-center justify-center px-4 py-6 border border-gray-100 rounded-xl"
+                                            className="flex flex-col items-center justify-center px-4 py-6 rounded-xl  border border-[#64748B]"
                                         >
                                             <img src={skills.image} className="w-8 h-8" />
-                                            <span className="mt-2 text-sm dark:text-[#9F9F9F]">{skills.name}</span>
+                                            <span className="mt-2">{skills.name}</span>
                                         </div>
                                     ))}
                                 </>
@@ -88,14 +87,14 @@ const Skills = () => {
                                     {tools.map((tool, index) => (
                                         <div
                                             key={index}
-                                            className="flex flex-col items-center justify-center px-4 py-6 border border-gray-100 rounded-xl"
+                                            className="flex flex-col items-center justify-center px-4 py-6 rounded-xl border border-[#64748B]"
                                         >
                                             {tool.image ? (
                                                 <img src={tool.image} alt={tool.name} className="w-8 h-8" />
                                             ) : (
                                                 tool.icon
                                             )}
-                                            <span className="mt-2 text-sm dark:text-[#9F9F9F]">{tool.name}</span>
+                                            <span className="mt-2">{tool.name}</span>
                                         </div>
                                     ))}
                                 </>
