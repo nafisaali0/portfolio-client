21af80128d339f5b0b4ec05e3d8d614fc40730fe
nafisaali0 <nafisaali20006@gmail.com>
2025-09-26 09:21:08 +0600

    change resume link

--- src/components/Link/LinkStore.js
+++ src/components/Link/LinkStore.js
@@ -2,7 +2,7 @@ export const LINKS = {
   linkedIn: "https://www.linkedin.com/in/ali-nafisa",
   github: "https://github.com/nafisaali0",
   resume:
-    "https://drive.google.com/file/d/1o3hwnJVF8OYaut87IRqsULorL5eHs8N6/view?usp=sharing",
+    "https://drive.google.com/file/d/1kSa0nEYajEno_hsdW7Hfqqz6zqrawdKV/view?usp=sharing",
   email: "https://mail.google.com/mail/?view=cm&fs=1&to=nafisaali20006@gmail.com",
   whatsapp:"https://wa.me/8801880857540"
 };
